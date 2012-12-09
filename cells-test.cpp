f62ea00037a74f46bb520ae1bb0d549e73474ace
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2012-12-09 10:18:45 +0100

    Apply LGPLv3.

--- cells-test.cpp
+++ cells-test.cpp
@@ -1,7 +1,22 @@
 // Copyright 2012, Matthias Andreas Benkard.
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
 
 #include "cells.hpp"
 
+#include <cstdlib>
 #include <memory>
 #include <iostream>
 
@@ -15,6 +30,7 @@ typedef formula_cell<double> fcell;
 typedef shared_ptr<fcell>    sfcell;
 typedef formula_cell<unit>   ucell;
 typedef shared_ptr<ucell>    sucell;
+
 
 int main(int argc, char** argv) {
   sfcell x0 = fcell::make();
@@ -40,6 +56,8 @@ int main(int argc, char** argv) {
   y->reset(-3);
   x1->reset([=]() { return (double)*x0; });
   y->reset([=]() { return *x1 + *x2; });
+
+  return EXIT_SUCCESS;
 }
 
 
